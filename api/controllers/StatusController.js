7a2bbddcc3f6b126a75362747da1caeff9779375
Nathan <nwmarsh1992@gmail.com>
2019-08-31 11:31:23 -0400

    added endpoint for status

---
+++ api/controllers/StatusController.js
@@ -0,0 +1,13 @@
+/**
+ * StatusController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  get: async function(req, res){
+        res.ok('Service is running');
+  } 
+};
+
