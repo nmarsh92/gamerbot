c056e88911a56079450a311d7778b4c917c5feb8
Nathan <nwmarsh1992@gmail.com>
2019-08-31 11:18:53 -0400

    add server greeting

--- api/services/DiscordService.js
+++ api/services/DiscordService.js
@@ -7,6 +7,15 @@ module.exports = {
       CommandService.execute(msg);
     });
 
+    client.on('guildMemberAdd', member => {
+      // Send the message to a designated channel on a server:
+      const channel = member.guild.channels.find(ch => ch.name === 'member-log');
+      // Do nothing if the channel wasn't found on this server
+      if (!channel) return;
+      // Send the message, mentioning the member
+      channel.send(`Welcome to the server, ${member}`);
+    });
+
     client.login(TokenService.discord);
   },
   reply: function (message, reply) {
