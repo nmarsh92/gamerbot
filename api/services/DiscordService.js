24adce0bed103f69df8479db72540a5601e8bdfa
Nathan <nwmarsh1992@gmail.com>
2020-02-20 09:33:26 -0500

    Update DiscordService.js

--- api/services/DiscordService.js
+++ api/services/DiscordService.js
@@ -15,7 +15,7 @@ module.exports = {
 
     client.on('guildMemberAdd', member => {
       // Send the message to a designated channel on a server:
-      const channel = member.guild.channels.find(ch => ch.name === 'landing-channel');
+      const channel = member.guild.channels.find(ch => ch.name === 'general');
       // Do nothing if the channel wasn't found on this server
       if (!channel) return;
       // Send the message, mentioning the member
